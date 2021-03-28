2bb38fd4a4041819fe204e33bdd9210eba7a4348
Sandra Geovany <sandra.geovany@gmail.com>
2021-03-28 15:42:02 +0100

    done

--- src/movies.js
+++ src/movies.js
@@ -1,7 +1,7 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors. How could you "clean" a bit this array and make it unified (without duplicates)?
-const getAllDirectors = ((arr) =>{
-    const newArr= arr.map((movie) =>{
+const getAllDirectors = ((arr) => {
+    const newArr = arr.map((movie) => {
         return movie.director;
     })
     return newArr;
@@ -10,7 +10,7 @@ const getAllDirectors = ((arr) =>{
 function howManyMovies(arr) {
     const spielMovies = arr.filter((movie) => {
         return movie.director === 'Steven Spielberg' &&
-        movie.genre.includes('Drama')
+            movie.genre.includes('Drama')
     });
     return spielMovies.length;
 
@@ -29,17 +29,17 @@ const ratesAverage = (arr) => {
 
             return accumulator;
         }
-        
+
     }, 0)
 
-    return Number((rateSum/arr.length).toFixed(2));
+    return Number((rateSum / arr.length).toFixed(2));
 }
 // Iteration 4: Drama movies - Get the average of Drama Movies
-const dramaMoviesRate = ((arr) =>{
-//Filter
-const dramaMovies= arr.filter((movie) =>{
-    return movie.genre.includes('Drama');
-});
+const dramaMoviesRate = ((arr) => {
+    //Filter
+    const dramaMovies = arr.filter((movie) => {
+        return movie.genre.includes('Drama');
+    });
 
     return ratesAverage(dramaMovies);
 
@@ -47,7 +47,36 @@ const dramaMovies= arr.filter((movie) =>{
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 
+const orderByYear= ((movies)=> {
+    const sortedMovies = movies.sort((a, b) => (a.year > b.year) ? 1 : -1);
+
+    
+
+    return sortedMovies;
+});
+
+
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
+const orderAlphabetically= ((movies)=> {
+    console.log(movies);
+    const sortedMovies = [];
+    movies.map((movie) => {
+        sortedMovies.push(movie.title) ;
+    })
+
+    sortedMovies.sort((a, b) => (a > b) ? 1 : -1);
+console.log(sortedMovies);
+const newArr=[];
+    if(sortedMovies.length<= 20){
+        return sortedMovies;
+    }else {
+        for(let i=0; i<20;i++){
+            newArr.push(sortedMovies[i]);
+        }
+        return newArr;
+    }
+    
+});
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 
